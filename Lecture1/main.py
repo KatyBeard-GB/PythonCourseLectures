112b95874188f15925620911a2a89ecb1d37acef
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-02-16 17:17:11 +0500

    Added folder 'Lecture1'

---
+++ Lecture1/main.py
@@ -0,0 +1,34 @@
+# print(5, 8, 6)
+# n = 1.86
+
+# print(n)
+
+# n = 'feje'
+
+# print(n)
+
+# n1 = "dljef"
+
+# n = 5
+# print(type(n))
+
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print(a, ' - ', b, ' - ', c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+
+# print('Введите первое число: ')
+# a = input()
+
+# b = input('Введите второе число: ')
+
+# print(f"{a} + {b} = {int(a)+int(b)}")
+
+a = 5.86515
+b = 6.576413
+print(round(a * b, 3))
+
