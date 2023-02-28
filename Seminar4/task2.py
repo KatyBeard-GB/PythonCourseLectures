8141a911e29ed7c7100cb145e275c388fbe1d780
KatyBeard <borodashkina.ekaterina@yandex.ru>
2023-03-01 00:36:07 +0500

    Added tasks for Seminar4

---
+++ Seminar4/task2.py
@@ -0,0 +1,14 @@
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# Output: 13
+
+text = input('Введите текст(строку): ').lower()
+
+print(f'Различных слов в тексте {len(set(text.split()))}')
